b693977bba85433cbf413a66d28ffee01e3e6558
heqing <1163003537@qq.com>
2023-08-11 10:55:33 +0800

    解决打包文件过大问题

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import DefineOptions from 'unplugin-vue-define-options/vite'
-import { resolve } from "path"
+import DefineOptions from "unplugin-vue-define-options/vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,6 +17,13 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    /**顶级await打包报错 */
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
   ],
   resolve: {
     alias: {
@@ -24,12 +32,29 @@ export default defineConfig({
   },
   server: {
     port: 8080,
-    host: "0.0.0.0"
+    host: "0.0.0.0",
   },
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@import "./src/assets/scss/index.scss";`, // 此处全局的scss文件
+      },
+    },
+  },
+  build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
       },
     },
   },
